37c34449ae40612f1c1c18d122210cec43689bdb
Alex Lorenz <alexlorenz95@gmail.com>
2020-01-09 11:45:56 -0300

    aula - 9/1/20

--- app/src/main/java/com/example/myapplication2/tela3.java
+++ app/src/main/java/com/example/myapplication2/tela3.java
@@ -1,6 +1,8 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
 
 import android.os.Bundle;
 
@@ -10,5 +12,8 @@ public class tela3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela3);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
