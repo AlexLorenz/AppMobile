6e68f0f3e6fe81ca65abc63fe8a9c04d9db110a5
Alex Lorenz <alexlorenz95@gmail.com>
2020-01-09 09:14:17 -0300

    correções de layout e criação da tela3

--- app/src/main/java/com/example/myapplication2/tela2.java
+++ app/src/main/java/com/example/myapplication2/tela2.java
@@ -1,7 +1,8 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class tela2 extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class tela2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
+    }
+    public void proximaTela(View view) {
+        Intent intent = new Intent(this, tela3.class);
+        startActivity(intent);
     }
 }
