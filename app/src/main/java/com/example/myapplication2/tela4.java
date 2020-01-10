93a4420a947a1f5e5bda0408c5546e0310c227f2
Alex Lorenz <alexlorenz95@gmail.com>
2020-01-10 11:32:16 -0300

    criando tela 4 e reutilizando toolbar

--- app/src/main/java/com/example/myapplication2/tela4.java
+++ app/src/main/java/com/example/myapplication2/tela4.java
@@ -7,18 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class tela3 extends AppCompatActivity {
+public class tela4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela3);
+        setContentView(R.layout.activity_tela4);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-    }
-    public void proximaTela(View view) {
-        Intent intent = new Intent(this, tela4.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication2/toolbar.java
+++ app/src/main/java/com/example/myapplication2/toolbar.java
@@ -9,6 +9,6 @@ public class toolbar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_toolbar);
+        setContentView(R.layout.toolbar);
     }
 }
