ce98e2d6136deb77c7c4814da49d469f6fa78f47
Alex Lorenz <alexlorenz95@gmail.com>
2020-01-13 11:00:02 -0300

    Adicionando Splashscreen

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,18 +2,28 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.Handler;
 import android.view.View;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Handler handler = new Handler();
+//        handler.postDelayed(this,2000);
+//        handler.postDelayed(this, 2000);
     }
     public void proximaTela(View view) {
         Intent intent = new Intent(this, tela2.class);
         startActivity(intent);
     }
+//    @Override
+//    public void run() {
+//        startActivity(new Intent(this, Splashscreen.class));
+//        finish();
+//    }
 }
---
+++ app/src/main/java/com/example/myapplication2/Splashscreen.java
@@ -0,0 +1,30 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Handler;
+
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+        private void mostrarMainActivity() {
+            Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    @Override
+    public void run() {
+            mostrarMainActivity();
+        }
+        },2000);
+    }
+}
