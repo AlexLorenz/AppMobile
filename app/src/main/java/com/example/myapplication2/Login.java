52af6b8de55116f6470d690381b14eb3c9f595c4
Alex Lorenz <alexlorenz95@gmail.com>
2020-01-27 08:47:06 -0300

    update project

--- app/src/main/java/com/example/myapplication2/Login.java
+++ app/src/main/java/com/example/myapplication2/Login.java
@@ -33,7 +33,7 @@ public class Login extends AppCompatActivity {
 
                 if(strEmail.equals(resEmail) && strSenha.equals(resSenha)) {
                     Toast.makeText(this, "Seja bem vindo.", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(this, Drawer.class);
+                    Intent intent = new Intent(this, MapsActivity.class);
                     startActivity(intent);
                 } else {
                     erro = true;
