aca8ec9edbd293be9f9648149a77a7697e7557dc
Alex Lorenz <alexlorenz95@gmail.com>
2020-01-28 09:11:12 -0300

    teste

--- app/src/main/java/com/example/myapplication2/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/example/myapplication2/ui/gallery/GalleryViewModel.java
@@ -1,19 +1,22 @@
-package com.example.myapplication2.ui.gallery;
-
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class GalleryViewModel extends ViewModel {
-
-    private MutableLiveData<String> mText;
-
-    public GalleryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
-}
+//package com.example.myapplication2.ui.gallery;
+//
+//import android.widget.Button;
+//
+//import androidx.lifecycle.LiveData;
+//import androidx.lifecycle.MutableLiveData;
+//import androidx.lifecycle.ViewModel;
+//
+//public class GalleryViewModel extends ViewModel {
+//
+//    private MutableLiveData<String> mText;
+//
+//    public GalleryViewModel() {
+//        mText = new MutableLiveData<>();
+//        mText.setValue("This is gallery fragment");
+//    }
+//
+//
+//    public LiveData<String> getText() {
+//        return mText;
+//    }
+//}
