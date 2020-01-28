de2445afc9ea72014ee85ae5836cd0c0da82e83c
Alex Lorenz <alexlorenz95@gmail.com>
2020-01-28 09:13:53 -0300

    teste

--- app/src/main/java/com/example/myapplication2/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication2/ui/gallery/GalleryFragment.java
@@ -11,9 +11,23 @@ public class GalleryFragment extends AppCompatActivity {
 
 //    private GalleryViewModel galleryViewModel;
 
-    @Override
-    public View onCreateView(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_gallery);
+//    @Override
+//    public View onCreateView(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_gallery);
+
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
+//        galleryViewModel =
+//                ViewModelProviders.of(this).get(GalleryViewModel.class);
+//        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+//        final TextView textView = root.findViewById(R.id.text_gallery);
+//        galleryViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+//        return root;
     }
 }
